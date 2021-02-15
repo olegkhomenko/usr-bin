443ca67a76b7b4d5865c15c0b73e507ee7afcbed
olegkhomenko <olegkhomenko@users.noreply.github.com>
2021-02-15 12:43:12 +0300

    added date param

--- hackernews-scrap.py
+++ hackernews-scrap.py
@@ -1,13 +1,24 @@
 #!/Users/ko/miniconda3/bin/python3
-# modify shebang with your python executable 
+# modify shebang with your python executable
+import argparse
+from typing import Optional
+
 import requests
 from bs4 import BeautifulSoup
 
 
-def hackernews_scrap():
-    resp = requests.get("https://news.ycombinator.com")
+def hackernews_scrap(date: Optional[str] = None):
+    if date is not None:
+        resp = requests.get(
+            "https://news.ycombinator.com/front",
+            params={"day": f"20{date[:2]}-{date[2:4]}-{date[4:6]}"},
+        )
+    else:
+        resp = requests.get("https://news.ycombinator.com")
+
     if not resp.ok:
         print(f"Something went wrong. Response status code: {resp.status_code}")
+        return
 
     html_text = resp.text
     soup = BeautifulSoup(html_text, "html.parser")
@@ -23,5 +34,20 @@ def hackernews_scrap():
         print(post.attrs.get("href") + "\n")
 
 
+def date_t(inp: str):
+    if len(inp) != 6 or not inp.isdigit():
+        raise ValueError("Date format should be: YYMMDD")
+    return inp
+
+
 if __name__ == "__main__":
-    hackernews_scrap()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--date",
+        default=None,
+        type=date_t,
+        help="Date to scrap in format: YYMMDD",
+    )
+    args = parser.parse_args()
+    hackernews_scrap(date=args.date)
